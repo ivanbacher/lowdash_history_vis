da3409c67341840d6bfe5e08fc1dfb27f2a13973
Ivan <ivanbacher@users.noreply.github.com>
2017-03-22 15:44:29 +0000

    save

--- vis/main.js
+++ vis/main.js
@@ -1,1 +1,7 @@
-console.log('hello');
+import * as d3 from 'd3';
+import esprima from 'esprima';
+import * as escope from 'escope';
+
+// 1) load the source code file
+// 2) parse the file using esprima and escope
+// 3) visualise the scope chain tree using d3 parcked circle layout
